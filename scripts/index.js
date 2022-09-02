605709c9e9d21f230796e58364b9880b24ce27fe
Santi <xxsmating@gmail.com>
2022-09-02 13:28:23 -0300

    ..

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,9 @@ let bars = document.getElementById('bars');
 bars.addEventListener('click',e => {
     ulNav.classList.toggle('ul-nav-show');
 })
+
+let ul_li_nav = document.getElementById('ul-li-nav');
+let ul_li_nav_ul = document.getElementById('ul-li-nav-ul');
+ul_li_nav.addEventListener('click', e =>{
+    ul_li_nav_ul.classList.toggle('ul-li-nav-ul-show');
+})
